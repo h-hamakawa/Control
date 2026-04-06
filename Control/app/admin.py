b8f08f00f9a9cda35fd421c45e3a448e7b2de852
h-hamakawa <fko2447005@stu.o-hara.ac.jp>
2026-04-06 13:09:59 +0900

    app admin.py add

--- Control/app/admin.py
+++ Control/app/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, UserProfile, Category, CreditCard, Transaction, Debt, CalendarEvent
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['email', 'name', 'is_active', 'is_staff']
+    list_filter = ['is_active', 'is_staff']
+    search_fields = ['email', 'name']
+    ordering = ['email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('個人情報', {'fields': ('name',)}),
+        ('権限', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2'),
+        }),
+    )
+    filter_horizontal = ('groups', 'user_permissions',)
+
+
+admin.register(UserProfile)(admin.ModelAdmin)
+admin.register(Category)(admin.ModelAdmin)
+admin.register(CreditCard)(admin.ModelAdmin)
+admin.register(Transaction)(admin.ModelAdmin)
+admin.register(Debt)(admin.ModelAdmin)
+admin.register(CalendarEvent)(admin.ModelAdmin)
